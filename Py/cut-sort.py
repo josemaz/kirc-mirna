64e7c45226e5daa7dad3bb7e19c988ca131e919b
Jose Maria Zamora <josema.genomics@gmail.com>
2021-03-04 11:09:08 -0600

    Update: Pearson

--- Py/cut-sort.py
+++ Py/cut-sort.py
@@ -8,8 +8,8 @@ for t in tipos:
 	fin = 'Output/peaval-'+ t +'.tsv'
 	utils.logprint(fin)
 	df = pd.read_csv(fin, sep = "\t")
-	df['P'] = df['P'].abs()
-	df = df.sort_values('P', ascending=False)
+	df['Pabs'] = df['P'].abs()
+	df = df.sort_values('Pabs', ascending=False)
 	fout = fin.split('.')[0] + '-' + \
 		utils.sizeof_fmt(nl) + '.tsv'
 	df.head(nl).to_csv(fout, 
